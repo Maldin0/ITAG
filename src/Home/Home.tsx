068521dabc109c07d09a5965ef342f7c7bc914e0
65070014 <112687540+65070014@users.noreply.github.com>
2023-09-12 18:22:34 +0000

    add Web Border

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,34 +1,51 @@
 import React from 'react';
 import logo from './logo.svg';
-import './Home.css';
+import Homestyle from './HomeStyle.module.css';
+import backgroundImage from './Images/village01.png';
 
-const bottomText = {
-  position: 'absolute' as const,
-  top: '10%',
-  left: '10%'
+const containerStyle = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100vh', // ความสูงเต็มหน้าจอ
+  borderLeft: '250px solid black', // ขอบด้านซ้ายสีดำ
+  borderRight: '250px solid black', // ขอบด้านขวาสีดำ
 };
 
-const centerText = {
-  textAlign: 'center' as const, 
-};
 
-const Title = () => (
+const ProjName = () => (
   
-  <div style={bottomText}>
-    <div style={centerText}>
-      <p style={{marginLeft: '-70%',font-size:'50px'}}>
+  <div style={{position: 'absolute',top: '17%', left: '21%'}}>
+    <div style={{textAlign: 'center'}}>
+      <h1 style={{ marginLeft: '-7%', fontSize: '70px'}}>
         IT<br/>
         ADVENTURER<br />
         guild
-      </p>
+      </h1>
     </div>
   </div>
 );
 
 function Home() {
+  /*const containerStyle = {
+    backgroundImage: `url(${backgroundImage})`, // ใช้รูปภาพเป็นพื้นหลัง
+    backgroundSize: 'cover', // ปรับขนาดรูปให้ครอบคลุมพื้นที่
+    backgroundPosition: 'center', // ตำแหน่งรูปภาพในหน้าเว็บ (ในกรณีนี้จะกลาง)
+    width: '100%',
+    height: '100vh', // ความสูงเต็มหน้าจอ
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+   };*/
+
+
   return (
-    <div>
-      <Title/>
+    <div style={containerStyle}>
+      <ProjName/>
+      <div style={{position: 'absolute',top: '35%', right:'18%'}}>
+        <h1 style={{ marginLeft: '-7%',fontSize: '70px'}}>Roll Your Dice</h1>
+      </div>
+
     </div>
   );
 }
