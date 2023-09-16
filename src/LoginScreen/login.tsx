8edc03cab2d2485b6447a3eaa14d404db0f54c36
Mr.FriendZoneSAD <78544214+FriendZone-SAD@users.noreply.github.com>
2023-09-16 23:29:14 +0700

    login ui that doesn't finish but i so sleepy so i will go to bed for now and tomorrow i will fight with it again

--- src/LoginScreen/login.tsx
+++ src/LoginScreen/login.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
 import loginStyle from './loginStyle.module.css'  ;
 import line from './images/line.png';
+import {log} from "util";
 
 function login() {
-  return (
-    
-      <div className={loginStyle.myImages}>
-      <div className={loginStyle.scroll}>
-        <div  style={{position: 'relative', display:'flex'}}> 
-          <img src={line} style={{width: '120em', height: ' 5em'}}></img>
-          <h1 className={loginStyle.topfont} style={{position:'absolute',top: '20%',left: '3%', color: 'White'}}>ITAG</h1>
+    return (
+
+        <div className={loginStyle.myImages}>
+            <div className={loginStyle.scroll}>
+                <div style={{position: 'relative', display: 'flex'}}>
+                    <img src={line} style={{width: '120em', height: ' 5em'}}></img>
+                    <h1 className={loginStyle.topfont}
+                        style={{position: 'absolute', top: '20%', left: '3%', color: 'White'}}>ITAG</h1>
+
+                </div>
+
+                <div className={loginStyle.box}>
+                    <h1>IT ADVENTURERS<br/>GUILD</h1>
+                    <div className={loginStyle.inputBox}>
+                        <input placeholder='E-mail/Username' type='input'></input>
+                    </div>
+                    <div className={loginStyle.inputBox}>
+                        <input placeholder='Password' type='input'></input>
+                    </div>
+                    <div className={loginStyle.submit}>
+                        <button type={'submit'}>Login</button>
+                    </div>
+                    <div className={loginStyle.line}></div>
+                    <div className={loginStyle.supButton}>
+                        <button type={'submit'}>Are you new adventurer?</button>
+                        <button type={'submit'}>Forgot password?</button>
+                    </div>
+                </div>
+
+            </div>
         </div>
 
-        <div style={{height: '100%' ,position: 'relative',display: 'block'} }>
-          <input placeholder='dawd' type='input'>
-         </input>
-        </div>
-        
-      </div>  
-    </div>
-
-  );
+    );
 }
 
 export default login;
