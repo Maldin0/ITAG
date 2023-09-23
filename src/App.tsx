a6f76ed35ec5f8a23cf2592129488de7c40f092d
Madino <65070015@kmitl.ac.th>
2023-09-23 16:13:25 +0700

    Add user session managemet in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState }from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './Home/Home';
 import Login from './LoginScreen/Login';
 import Regis from './RegisScreen/Regis';
 
 function App() {
+    const [user, setUser] = useState(null);
+    
+    useEffect(() => {
+        const userFromStorage = localStorage.getItem('user');
+        if (userFromStorage) {
+            setUser(JSON.parse(userFromStorage))
+        }
+    }, [])
+    
+    
     return (
         <Router>
             <Routes>
