5a716fcfd630109363975472c93c81a4a2411193
Mr.FriendZoneSAD <78544214+FriendZone-SAD@users.noreply.github.com>
2023-09-24 11:41:31 +0700

    เพิ่ม getItem ใน inventory แล้วแต่ไม่รู้ทำถูกไหม

--- src/scripts/Inventory.ts
+++ src/scripts/Inventory.ts
@@ -1,4 +1,16 @@
 import Item from './Item';
+import {IDatabase} from "pg-promise";
+
+
+const pgPromise = require('pg-promise');
+
+const db : IDatabase<unknown>= pgPromise()({
+    host: '161.246.127.24',
+    port: 9077,
+    database: 'dbitag',
+    user: 'clmtbmrw30079bsmnfdwi4ovp',
+    password: 'YcVOt4I2p6X3YTDXNltyKgxN'
+});
 
 export default class Inventory {
     items: Item[];
@@ -7,5 +19,23 @@ export default class Inventory {
     constructor(items: Item[], gold: number) {
         this.items = items;
         this.gold = gold;
+    }
+
+    async getitem() {
+        try {
+            await db.tx(async (t)=>{
+                const query = 'select * from inventories i natural join items it'
+                const value = [this.items, this.gold]
+                const invendata = await t.many(query, value)
+                for(const inven of invendata)
+                {
+                    const temp = new Item(inven.item_name, inven.item_detail)
+                    this.items.push(temp)
+                }
+            })
+        }
+        catch (error) {
+            console.error(error)
+        }
     }
 }
